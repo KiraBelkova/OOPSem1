23cdcaa26607cdf669b7f7b15ac78da560afa864
Кира Белькова <kirabelkova2013@yandex.ru>
2024-03-13 23:53:01 +0300

    добавила класс HotDrinkWithTemperature

---
+++ HotDrinkWithTemperature.java
@@ -0,0 +1,24 @@
+//Создаем наследника класса ГорячийНапиток - класс ГорячийНапитокСТемпературой:
+
+public class HotDrinkWithTemperature extends HotDrink {
+    private int temperature;
+
+    public HotDrinkWithTemperature(String name, int volume, int temperature) {
+        super(name, volume);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "HotDrinkWithTemperature{" +
+                "name='" + getName() + '\'' +
+                ", volume=" + getVolume() +
+                ", temperature=" + temperature +
+                '}';
+    }
+}
+
